b3276e7f2a288180220633db4cf43318456c8e5f
Bryan Coxwell <bryancoxwell@gmail.com>
2021-06-05 13:43:38 -0400

    Add option to open PySim-shell.

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# On Linux, install dependencies: apt-get install -y pcscd pcsc-tools libccid libpcsclite-dev python-pyscard
+# Install dependencies: apt-get install -y pcscd pcsc-tools libccid libpcsclite-dev python-pyscard
 git clone https://github.com/osmocom/pysim
 pushd pysim
 python3 -m venv env
@@ -9,12 +9,10 @@ pip install wheel
 pip install -r requirements.txt
 deactivate
 popd
-# run with eg: pysim/env/bin/python3 pysim/pySim-read.py -p 0
+
 git clone https://github.com/herlesupreeth/sim-tools
 pushd sim-tools
 virtualenv -p python2 env
 source env/bin/activate
 pip install pycrypto pyscard
 popd
-
-# run with eg: sim-tools/env/bin/python sim-tools/shadysim/shadysim.py --pcsc -t
